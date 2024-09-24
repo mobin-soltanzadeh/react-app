65e5df2a95fc61ad2ce5bb7f82de81457e124018
mobin <mobin.soltanzadeh.work@gmail.com>
2024-09-24 17:57:55 +0330

    ..

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     return (
         <div className="App">
             <h1 className="bg-sky-600">salam</h1>
+            <h1 className="bg-sky-600">mobin</h1>
         </div>
     );
 }
